4aaaece186fb7a10a9d447a52886759e2a5c0c8d
nuevo-rama <ramiromelero@gmail.com>
2021-02-26 17:09:03 -0300

    agrego funcionalidad a controllers

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -20,6 +20,21 @@ const productosController = {
     productFormEdit: (req,res)=>{
         res.render("./products/productFormEdit")
     },
+
+    //----------------------------------------------
+
+    detalleProducto: function (req,res) {
+        res.send ("Bienvenidos al detalle de un producto" + req.params.idProducto)
+    },
+
+    detalleProductoComentario: function (req,res) {
+        if (req.params.idComentario==undefined) {
+            res.send ("Bienvenidos al detalle de un producto" + req.params.idProducto); 
+        } else {
+            res.send ("Bienvenidos al comentario del producto" + req.params.idProducto + 
+            "y estás en el comentario " + req.params.idComentario); 
+        }
+    }
 }
 
 module.exports=productosController; //se exportará hacia routes
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,10 +5,13 @@ const productosController = require("../controllers/productosController");
 
 const router = express.Router();
 
-router.get("/listadoProductos", productosController.index);
+router.get("/listadoProductos", productosController.index); //accedemos al método que queremos que sea ejecutado al llamar a la ruta
 router.get("/product", productosController.product);
 router.get("/ShoppingCart", productosController.shoppingCart);
 router.get("/productFormNew", productosController.productFormNew);
 router.get("/productFormEdit", productosController.productFormEdit);
+
+router.get("/:idProducto", productosController.detalleProducto); //va a mostral el detalle de un producto por id
+router.get("/:idProducto/comentarios", productosController.detalleProductoComentario); //va a mostrar el comentario de un producto por id
 
 module.exports = router;
